444e9ab0655801b777ddac9c4d31d3e91ee7558b
jiwon-yang <jiwonyang.tech@gmail.com>
2019-11-22 15:11:41 +0900

    Update Tab Navigation and Fix Version Error

--- navigation/AuthNavigation.js
+++ navigation/AuthNavigation.js
@@ -1,5 +1,6 @@
 import { createAppContainer } from "react-navigation"; //여기 바꾼거
 import { createStackNavigator } from "react-navigation-stack"; //여기 바꾼거
+// import { createStackNavigator, createAppContainer } from "react-navigation";
 import Signup from "../screens/Auth/Signup";
 import Confirm from "../screens/Auth/Confirm";
 import Login from "../screens/Auth/Login";
@@ -7,7 +8,6 @@ import AuthHome from "../screens/Auth/AuthHome";
 
 const AuthNavigation = createStackNavigator(
     {
-        //routes
         AuthHome,
         Signup,
         Login,
