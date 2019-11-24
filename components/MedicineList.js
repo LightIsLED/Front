3f1d7d6df865a0bc2fa59e35c5065821ebd44725
jiwon-yang <jiwonyang.tech@gmail.com>
2019-11-24 23:19:44 +0900

    Update MedicineList UI

--- components/MedicineList.js
+++ components/MedicineList.js
@@ -7,6 +7,7 @@ import { Text, View, StyleSheet, FlatList } from "react-native";
 const MedicineList = ({ data }) => {
     return (
         <View style={styles.container}>
+            <Text style={styles.title}>복용 중인 약</Text>
             <FlatList
                 data={data}
                 renderItem={({ item }) => (
@@ -21,16 +22,22 @@ const MedicineList = ({ data }) => {
 };
 
 const styles = StyleSheet.create({
+    title: {
+        margin: 20,
+        fontSize: 22
+    },
     medicine: {
-        marginTop: 30,
-        marginBottom: 0,
+        marginTop: 0,
+        marginBottom: 20,
         marginLeft: 50,
         marginRight: 50,
         borderWidth: 0.5,
-        borderRadius: 50,
+        borderRadius: 30,
         width: "75%"
     },
     item: {
+        //borderWidth: 0.5,
+        //borderRadius: 50,
         margin: 10,
         padding: 10,
         fontSize: 18,
